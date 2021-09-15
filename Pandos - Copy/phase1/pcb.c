df5f7636c470ae0d9f3cbd1fd275411d66c95b54
weeksc2 <weeksc2@xavier.edu>
2021-09-15 01:21:37 -0400

    Updates to pcb.c
    Updated comments and fixed up some syntax

--- Pandos - Copy/phase1/pcb.c
+++ Pandos - Copy/phase1/pcb.c
@@ -4,7 +4,7 @@
 HIDDEN pcb_t *pcbFree_h;
 
 
-// Allocation and Deallocation Methods
+/* Allocation and Deallocation Methods */
 void freePcb(pcb_PTR p)
 {
 	insertProcQ(&(pcbFree_h), p);
@@ -37,15 +37,14 @@ void initPcbs()
 	}
 }
 
-// Process Queue Maintenance Methods
-pcb_PTR mkEmptyProcQ () // Done
+/* Process Queue Maintenance Methods */
+pcb_PTR mkEmptyProcQ () 
 {
 	return (NULL);
 }
 
-int emptyProcQ (pcb_PTR tp) // Done
+int emptyProcQ (pcb_PTR tp) 
 {
-	
 	if(tp == NULL)
 	{	return TRUE;	}
 	else
@@ -58,22 +57,22 @@ void insertProcQ (pcb_PTR tp, pcb_PTR p)
    	/* if q empty then tail pointer points to p */ 
 	if(emptyProcQ(tp))
 	{
-		p->p_next=tp; // Def not correct (will fix)
+		p->p_next=tp; /* Def not correct (will fix) */
 		tp=p;
 	}
-	//if not empty update tp to be 
+	/* if not empty update tp to be */
 	else
 	{
-		 p->p_next = tp->p_next;
-		 tp->p_next=p;
-		 tp=p
+		 p->p_next = (*tp)->p_next;
+		 (*tp)->p_next=p;
+		 tp=p;
 	}
 }
 pcb_PTR removeProcQ (pcb_PTR tp)
 {
 	
 	pcb_PTR temp = tp->p_next ;
-	//only one element
+	/* only one element */
 	return (outProcQ(tp, temp);
 }
 
@@ -85,50 +84,44 @@ pcb_PTR outProcQ (pcb_PTR tp, pcb_PTR p)
 	{
 		return NULL;
 	}
-	pcb_PTR temp = tp->p_next ;
-	
+	pcb_PTR temp = (*tp)->p_next;
 	if(temp == p){
 	
-		//only one element
+		/* only one element */
 		if(temp->p_next == NULL)
 		{
-		tp->p_next = NULL;
+		(*tp)->p_next = NULL;
 		tp = mkEmptyProcQ();
 		return temp;
 		}
-		tp->p_next = temp->p_next;
+		(*tp)->p_next = temp->p_next;
 		return temp;
 	}
 	for(int i = 0;i<MAXPROC;i++){
 		if(temp->p_next == p){
-			temp->p_next=p->next;//p->p_prev->p_next = p->p_next;
+			temp->p_next=p->next; /* p->p_prev->p_next = p->p_next; */
 			return p;
 		}
 		temp = temp->p_next; 
-		
 	}
-	
-	// p not found in queue
+	/* p not found in queue */
 	return NULL;
-	
-	
-	
-	
 }
 
 pcb_PTR headProcQ (pcb_PTR tp)
 {
+	pcb_PTR firstPCB;
 	pcb_PTR firstPCB=tp->p_next;
-	if(tp->tail != NULL)
+	if(tp->firstPCB != NULL) /* changed tail to firstPCB */
 	{	return firstPCB;	}
 	else
 	{	return NULL;		}
 }
 
-// Process Tree Maintenance Methods
+/* Process Tree Maintenance Methods */
 int emptyChild (pcb_PTR p)
 {
- 	// Return 1 if true. Else, return 0.
+ 	/* Return 1 if true. Else, return 0. */
  	if(p->p_child==NULL)
 	{
  		return TRUE;
@@ -142,12 +135,12 @@ int emptyChild (pcb_PTR p)
 void insertChild (pcb_PTR prnt, pcb_PTR p)
 {
 	pcb_PTR child = prnt->p_child;
-	// No children
+	/* No children */
 	if(child==NULL)
 	{
 		prnt->p_child = p;
 	}
-	// One or more children
+	/* One or more children */
 	p->p_sib = prnt->p_child;
 	prnt->p_child = p;
 	
@@ -160,18 +153,18 @@ pcb_PTR removeChild (pcb_PTR p)
 		return NULL; 
 	else
 	{
-		// Only one child
+		/* Only one child */
 		if(sib == NULL)
 		{
-			pcb_t removed = p->p_child;
-			p->p_child = NULL;
-			tp = NULL;
+			pcb_t removed = (*p)->p_child;
+			(*p)->p_child = NULL;
+			(*p) = NULL; /* changed tp to *p */
 			return removed;
 		}
 		
-		// More than one child
-		pcb_t removed = p->p_child;
-		p->p_child = sib;
+		/* More than one child */
+		pcb_t removed = (*p).p_child; /* changed -> to . */
+		(*p).p_child = sib; /* changed -> to . */
 		return removed;
 		
 	}
